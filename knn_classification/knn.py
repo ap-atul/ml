c0c33296ed25510f6b85fdc2a348fe1ac01b48bc
AP-Atul <atulpatare99@gmail.com>
2021-01-18 20:12:54 +0530

    knn added

---
+++ knn_classification/knn.py
@@ -0,0 +1,25 @@
+from collections import Counter
+from math import sqrt
+
+def euclidean(a, b):
+    return sqrt((a - b) ** 2)
+
+def accuracy(act, pred):
+    return (sum([1 for yt, yp in zip(act, pred) if yt == yp]) / len(pred))
+
+class Knn:
+    def __init__(self):
+        self.xtrain, self.ytrain = None, None
+
+    def fit(self, X_train, y_train):
+        self.xtrain, self.ytrain = X_train, y_train
+
+    def predict(self, X_test, k=5):
+        return [self.closet(row, k) for row in X_test]
+
+    def closet(self, row, k):
+        distances = [(i, euclidean(row, self.xtrain[i])) for i in range(len(self.xtrain))]
+        distances = sorted(distances, key=lambda x: x[1])[0: k]
+        k_indices = [distances[i][0] for i in range(k)]
+        k_labels = [self.ytrain[k_indices[i]] for i in range(k)]
+        return max(set(k_labels), key=k_labels.count)
