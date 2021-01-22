bdc4d6d913f9ed82a2e0efd5c29de0add133abb4
AP-Atul <atulpatare99@gmail.com>
2021-01-22 22:53:22 +0530

    minimize lr

--- linear_regression/lr.py
+++ linear_regression/lr.py
@@ -10,26 +10,17 @@ def variance(values, mean): # sum( (x - mean(x))^2 )
     return sum([(x - mean)** 2 for x in values])
 
 def covariance(x, y, x_mean, y_mean): # sum((x(i) - mean(x)) * (y(i) - mean(y)))
-    ret = 0.0
-    for i in range(len(x)):
-        ret += (x[i] - x_mean) * (y[i] - y_mean)
-    return ret
+    return sum([(x[i] - x_mean) * (y[i] - y_mean) for i in range(len(x))])
 
 class LinearRegression: # simple linear regression
     def __init__(self):
-        self._m = None
-        self._b = None
+        self._m, self._b = None, None
 
     def fit(self, X, y):
         self._m, self._b = self._coefficients(X, y)
-        print(f"Y-intercept :: {self._m}, Bias ::  {self._b}")
 
     def predict(self, X):
-        prediction = list()
-        for row in X:
-            y = self._b + self._m * row
-            prediction.append(y)
-        return prediction
+        return [(self._b + self._m * row) for row in X]
 
     def _coefficients(self, x, y):
         x_mean, y_mean = mean(x), mean(y)
