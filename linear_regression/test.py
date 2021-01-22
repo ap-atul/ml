ded8e6deddd6dc3d12ef01d111cb4497997f59dc
AP-Atul <atulpatare99@gmail.com>
2021-01-22 19:46:34 +0530

    change

--- linear_regression/test.py
+++ linear_regression/test.py
@@ -14,17 +14,17 @@ y = data.iloc[:, 1]
 X = np.array(X)
 y = np.array(y)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7)
 
-print(f"Training samples :: {len(X_train)}")
-print(f"Testing sample :: {len(X_test)}")
+#print(f"Training samples :: {len(X_train)}")
+#print(f"Testing sample :: {len(X_test)}")
 
 model = LinearRegression()
-model.fit(X_train, y_train)
-pred = model.predict(X_test)
+model.fit(X, y)
+pred = model.predict(X)
 
 plt.scatter(X, y, marker="*", color='g')
-plt.plot(X_test, pred, marker=".", color='r')
+plt.plot(X, pred, marker=".", color='r')
 plt.show()
 
-print(f"RMSE :: {rmse(y_test, pred)}")
+print(f"RMSE :: {rmse(y, pred)}")
