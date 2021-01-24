2b96c99bca58a74cf76fa18225117bb4c39a438b
AP-Atul <atulpatare99@gmail.com>
2021-01-24 23:04:00 +0530

    added plot

--- kmeans_clustering/test.py
+++ kmeans_clustering/test.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 from kmeans import *
 
 points = [[0.1,0.6], [0.15,0.71], [0.08,0.9], [0.16,0.85], [0.2,0.3], [0.25,0.5], [0.24,0.1], [0.3,0.2]]
@@ -5,7 +6,6 @@ centers = [[0.1, 0.6], [0.3, 0.2]]
 
 km = KMeans()
 km.fit(points)
-
 
 # 1. P6 belongs to cluster
 print(f"P6 point belongs to cluster:: {km.predict(points[5])}")
@@ -15,3 +15,15 @@ print(f"Population of cluster 2 :: {len(km._classifications[1])}")
 
 # 3. Updated values for the clusters
 print(f"Updated clusters :: {list(km._centroids.values())}")
+
+# plotting the cluster and centers
+colors = ['r', 'g']
+for classification in km._classifications:
+    for x, y in km._classifications[classification]:
+        plt.scatter(x, y, color=colors[classification], s=10)
+
+for center in km._centroids:
+    x, y = km._centroids[center]
+    plt.scatter(x, y, color=colors[center], marker="*", s=50)
+plt.show()
+
