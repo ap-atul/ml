1113a8281e5b7fdfd2637b8955d23b11ff1dde85
AP-Atul <atulpatare99@gmail.com>
2021-01-29 21:41:42 +0530

    test change

--- knn_classification/test.py
+++ knn_classification/test.py
@@ -8,16 +8,13 @@ X = df.iloc[:, :-1].values
 y = df['class'].values
 
 print(f"Training samples :: {len(X)}")
-
 model = Knn()
 model.fit(X, y)
 
 df = pd.read_csv("test.csv")
 X = df.iloc[:, :-1].values
 y = df['class'].values
-
 print(f"Testing sample :: {len(y)}")
 
 pred = model.predict(X)
-
 print(f"Accuracy score :: {accuracy(y, pred)* 100}")
