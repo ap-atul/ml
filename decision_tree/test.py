992140ea5d51aec4445908a232a26a9fb7606c18
AP-Atul <atulpatare99@gmail.com>
2021-01-30 19:20:51 +0530

    added test

---
+++ decision_tree/test.py
@@ -0,0 +1,11 @@
+from decision import *
+import numpy as np
+
+X_train = [[1.0, 3.2, 4.4, 5.2], [1.0, 1.9, 2.5, 5.2]]
+y_train = [[1], [0]]
+
+X_test = [[1.0, 1.8, 2.4, 5.1]]
+y_test = [[0]]
+
+classifier = DecisionTree()
+classifier.fit(np.array(X_train), np.array(y_train))
